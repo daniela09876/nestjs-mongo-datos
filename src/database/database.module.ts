834439a88b8c13e89467ab2007c56d416796382b
daniela09876 <70279267+daniela09876@users.noreply.github.com>
2024-01-30 18:55:26 +0000

    Fix issues in files and reinstall Mongoose

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
-import mongoose, { Mongoose } from 'mongoose'
+import { MongooseModule } from '@nestjs/mongoose'
 import { MongoClient } from 'mongodb';
 
 import config from '../config';
@@ -10,6 +10,13 @@ const API_KEY_PROD = 'PROD1212121SA';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/', {
+      user: 'root',
+      pass: 'root',
+      dbName: 'platzi-store',
+    })
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -18,15 +25,23 @@ const API_KEY_PROD = 'PROD1212121SA';
     {
       provide: 'MONGO',
       useFactory: async (configService: ConfigType<typeof config>) => {
-        const { uri, dbName } = configService.mongo;
-        await mongoose.connect(uri, {
-          dbName,
-        });
-        return mongoose.connection.db;
+        const {
+          connection,
+          user,
+          password,
+          host,
+          port,
+          dbName, } = configService.mongo;
+        const uri =
+          `${connection}://${user}:${password}@${host}:${port}/?authSource=admin`;
+        const client = new MongoClient(uri);
+        await client.connect();
+        const database = client.db(dbName);
+        return database;
       },
       inject: [config.KEY]
     }
   ],
-  exports: ['API_KEY', 'MONGO'],
+  exports: ['API_KEY', 'MONGO', MongooseModule],
 })
 export class DatabaseModule { }
