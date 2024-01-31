7b1196a24e08aa87bf469df7a49f35405df62b8e
daniela09876 <70279267+daniela09876@users.noreply.github.com>
2024-01-31 14:32:51 +0000

    Configure MongooseModule for Product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,23 @@
-export class Product {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose'
+
+@Schema()
+export class Product extends Document {
+
+  @Prop()
   name: string;
+
+  @Prop()
   description: string;
+
+  @Prop({ type: Number })
   price: number;
+
+  @Prop({ type: Number })
   stock: number;
+
+  @Prop()
   image: string;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
